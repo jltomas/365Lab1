99942a63dd96781a086bfd71124ef6e2a734badd
jltomas <jltomas@calpoly.edu>
2018-01-08 12:25:34 -0800

    Basic IO functionality

--- schoolsearch.java
+++ schoolsearch.java
@@ -3,4 +3,12 @@
  * @version Lab1
  */
 
+import java.util.*;
 
+class schoolsearch {
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      String input = scanner.nextLine();
+      System.out.println(input);
+   }
+}
