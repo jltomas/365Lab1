a4577a084f10cebe902f8ab941235dd861c61ff1
Victor Bodell <vbodell@kth.se>
2018-01-16 12:02:50 -0800

    Update & rename test suite

--- test.sh
+++ test.sh
@@ -3,17 +3,17 @@
 # @author Justin Tomas & Victor Bodell
 # @version Lab1
 #
-# Instructions: Run ./test.sh. You can adjust the commands.txt file
+# Instructions: Run ./test.sh. You can adjust the tests.txt file
 # to test the different expectations. The output will be redirected to
-# a file called output.txt. You can diff output.txt with expectoutput.txt.
+# a file called tests.out. You can diff tests.out with expected.out.
 # If the program is correct, there will be no difference.
 #
 #
 
 javac schoolsearch.java
-java schoolsearch < commands.txt > output.txt
+java schoolsearch < tests.txt > tests.out
 
-DIFF=$(diff output.txt expectedoutput.txt)
+DIFF=$(diff tests.out expected.out)
 if [ "$DIFF" != "" ]
 then echo "Something went wrong."
 else echo "All tests pass."
